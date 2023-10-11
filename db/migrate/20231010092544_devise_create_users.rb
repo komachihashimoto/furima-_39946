6c710bb84984076887d5c211fafa7daf048e891a
komachihashimoto <joujinameko@gmail.com>
2023-10-11 10:37:37 +0900

    usersテーブルの作成

--- db/migrate/20231010092544_devise_create_users.rb
+++ db/migrate/20231010092544_devise_create_users.rb
@@ -4,8 +4,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :name,               null: false, default: ""
+      t.string :email,              null: false, default: "", unique: true
       t.string :encrypted_password, null: false, default: ""
+      t.string :last_name,          null: false, default: ""
+      t.string :first_name,         null: false, default: ""
+      t.string :last_name_furigana, null: false, default: ""
+      t.string :first_name_furigana,null: false, default: ""
+      t.date   :dob,                null: false, default: "2000-01-01"
+      
 
       ## Recoverable
       t.string   :reset_password_token
