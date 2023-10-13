a9fa177e5ad3f1eeae42d9b4bfefbe86df321eac
komachihashimoto <joujinameko@gmail.com>
2023-10-13 18:00:42 +0900

    出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+  def index
+    @items = Item.all.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to "/"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :information, :category_id, :condition_id, :delivery_cost_id, :prefecture_id, :shipping_date_id, :price ).merge(user_id: current_user.id)
+  end
 end
---
+++ app/models/item.rb
@@ -0,0 +1,16 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_cost
+  belongs_to :prefecture
+  belongs_to :shipping_date
+  has_one_attached :image
+
+  validates :image, :name, :information, :price, presence: true
+  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price, format: { with: /\A[0-9]+\z/ }
+  validates :category_id, :condition_id, :delivery_cost_id, :prefecture_id, :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank" }
+end
+
