c1b7dd96af65f787b6e69c4ae2ba7086014977b6
komachihashimoto <joujinameko@gmail.com>
2023-10-17 12:07:09 +0900

    商品編集機能とユーザーの選別

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :redirect_to_root, except: [:index, :show]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -21,9 +22,31 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if
+      @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :information, :category_id, :condition_id, :delivery_cost_id, :prefecture_id, :shipping_date_id, :price ).merge(user_id: current_user.id)
   end
+
+  def redirect_to_root
+    @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to root_path
+    end
+  end
+
 end
